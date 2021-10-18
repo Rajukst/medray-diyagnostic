5139fc983977ddba57352281dd14ae23cff0bb53
Rajukst <rajuahamedkst15@gmail.com>
2021-10-18 16:40:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import Services from './Components/Service/Services';
+import NotFound from './Components/NotFound/NotFound';
+import About from './Components/About/About';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+          <Home></Home>
+          </Route>
+          <Route path="/home">
+          <Home></Home>
+          </Route>
+          <Route path="/doctors-list">
+
+          </Route>
+          <Route path="/service">
+          <Services></Services>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="*">
+          <NotFound></NotFound>
+          </Route>
+        </Switch>
+        <Footer/>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>Hello This is From Footer</p>
+        </div>
+    );
+};
+
+export default Footer;
